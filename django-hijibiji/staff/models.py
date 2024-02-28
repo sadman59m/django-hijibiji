0575586a9df785b2fa3ab6535c7d53aa51fcd6ba
Sadman Sakib <sadman@Sadmans-MacBook-Air.local>
2024-02-28 11:02:45 +0600

    added RunPython code in the 002_unit migration file

---
+++ django-hijibiji/staff/models.py
@@ -0,0 +1,12 @@
+# Standard library imports
+
+# Django imports
+from django.contrib.auth.models import User
+from django.db import models
+
+# Django Rest Framework imports
+
+# Third party imports
+
+# Local imports
+
---
+++ django-hijibiji/unit/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+# Standard library imports
+
+# Django imports
+from django.contrib.auth.models import User
+from django.db import models
+
+# Django Rest Framework imports
+
+# Third party imports
+
+# Local imports
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=255)
+    area = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    admins = models.ManyToManyField(
+        User, related_name="unit_admins"
+    )
+    staff = models.ManyToManyField(
+        User, related_name="unit_staff"
+    )
+
+    def __str__(self):
+        return self.name
